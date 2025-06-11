b9bf6c141a251df018fb0d051c2a91ca295acd04
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2025-06-11 14:00:49 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,9 @@
 
 This is the main script you should run when testing your code.
 
-If you want to take a peek into the engine script, feel free! I've added some
+If you want to take a peek into the engine script, feel free - just don't edit it! I've added some
 comments to help make some sense of my code, however if something confuses you let
 me know and I'll do my best to help!
-
-Please don't edit the engine script, if there are any problems or if you need some
-help, email me at theotsmith@protonmail.com. I may be away so it might be a little while
-before you get a response.
 
 """
 
